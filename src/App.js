304675300197285f3df1b46f527cd004a1ef1d55
iqkna <nikolay.kuznetsov@iqplanner.com>
2019-02-18 15:52:55 +0300

    - project page layout;

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React from 'react'
 import {Root, Routes} from 'react-static'
-import {createGlobalStyle, ThemeProvider} from 'styled-components'
+import {createGlobalStyle, ThemeProvider, css} from 'styled-components'
 import {theme} from "./theme";
 import {CloudinaryContext} from "cloudinary-react";
 import NavBar from "./organisms/NavBar";
+import {description} from './atoms'
+import listItem from './listItem.svg'
+
+const paragraph = css`
+  font-weight: ${theme.fontWeights[0]};
+  font-size:  ${theme.fontSizes[10]}px;
+  line-height: ${theme.lineHeight[10]}px;
+`
 
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: ${theme.fonts.base};
-    margin: 0;
+    margin: 0; 
     padding: 0;
     background: ${theme.colors.black[0]}
     color: ${theme.colors.white[0]}
     overflow-x: hidden;
+  }
+  
+  h2 {
+    ${description};
+    font-weight: ${theme.fontWeights[1]};
+    font-size:  ${theme.fontSizes[5]}px;
+    line-height: ${theme.lineHeight[5]}px;
+    margin: 60px 0 0 0;
+  }
+  
+  p {
+    font-weight: ${theme.fontWeights[0]};
+    font-size:  ${theme.fontSizes[10]}px;
+    line-height: ${theme.lineHeight[10]}px;
+  }
+  
+  ul {
+    list-style: none;
+    padding: 0;
+    li {
+      vertical-align: top;
+      color: ${theme.colors.gray[2]}
+      font-weight: ${theme.fontWeights[1]};
+      font-size:  ${theme.fontSizes[11]}px;
+      line-height: ${theme.lineHeight[11]}px;
+      background: url(${listItem}) no-repeat left top;
+      padding: 0 35px;
+      margin-bottom: 20px;
+      display: inline-block;
+      box-sizing: border-box;
+      width: 50%;
+    }
   }
 `
 
--- src/containers/Project.js
+++ src/containers/Project.js
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {withRouteData} from 'react-static'
 import Markdown from 'react-markdown'
-import {Container, H1} from "../atoms";
+import {Container, H1, Subtitle} from "../atoms";
 import {Image} from "cloudinary-react";
 import {Box} from '@rebass/grid'
-import {ServiceList} from "../components";
+import {Footer} from '../organisms'
+
+const serviceList = (serviceIds, services) => {
+    return services.filter(item => serviceIds && serviceIds.includes(item.data.slug)).
+    map(item => item.data.title).
+    join(', ');
+}
 
 export default withRouteData(({item, data}) => (
-    <Container>
-        <H1>{item.data.title}</H1>
-        <ServiceList services={data.services}
-                     includes={item.data.services}
-                     bg='gray.1'/>
+    <Fragment>
+        <Container>
+            <Box width={2/3}>
+                <H1>{item.data.title}</H1>
+                <Subtitle>{serviceList(item.data.services, data.services)}</Subtitle>
 
-        <Box width={1}>
-            <Image publicId={'site/project/' + item.data.cover}
-                   crop="fill"
-                   gravity='auto'
-                   width="auto"
-                   height="320"
-                   responsive
-                   responsive_use_breakpoints/>
-        </Box>
-        <Markdown source={item.content} escapeHtml={false}/>
-    </Container>
+                <Box mt={5} mb={5}>
+                    <Image  publicId={'site/project/' + item.data.cover}
+                       crop="fill"
+                       gravity='auto'
+                       width="auto"
+                       height="320"
+                       responsive
+                       responsive_use_breakpoints/>
+                </Box>
+
+                <Markdown source={item.content} escapeHtml={false}/>
+            </Box>
+            <Box width={1/3}>
+
+            </Box>
+        </Container>
+        <Footer mt={10} mb={6}/>
+    </Fragment>
 ))
