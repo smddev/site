afeba2be1fc29513f8a56666a0a2aade6fe3e88e
kna-github <nikolay.kuznetsov@gmail.com>
2019-10-15 19:22:50 +0300

    reviews on media queries;

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -5,7 +5,6 @@ import Review from '../components/Review';
 import styled from 'styled-components';
 import {withBackground} from "../atoms";
 import quote from "../quote.svg";
-import {responsive} from "../utils";
 
 const FeedbackList = withBackground(quote, 240, 160)(styled.div`
   width: 100%;
@@ -22,42 +21,42 @@ const StyledVerticalCarousel = styled(VerticalCarousel)`
   height: 442px;
   width: 566px;
   margin: 0 auto;
+  @media(max-width: ${p => p.theme.brkpnts[1] - 1}px) {
+    display: none;
+  }
 `;
 
 const StyledHorizontalCarousel = styled(Carousel)`
   height: 464px;
   width: 280px;
   margin: 0 auto;
+  @media(min-width: ${p => p.theme.breakpoints[1]}) {
+    display: none;
+  }
 `;
 
-const Reviews = ({reviews, className, isMobile}) => (
+const Reviews = ({reviews}) => (
   <FeedbackList>
-    {isMobile ? (
-      <StyledHorizontalCarousel
-        height={428}
-        width={280}
-        carousel={true}
-        alignItems="center"
-      >
-        {reviews.slice(0, 3).map((review, key) => (
-          <Review {...{review, key}} mb={3}/>
-        ))}
-      </StyledHorizontalCarousel>
-    ) : (
-      <StyledVerticalCarousel
-        height={448}
-        width={500}
-        carousel={true}
-        ml='45px'
-      >
-        {reviews.slice(0, 3).map((review, key) => (
-          <Review {...{review, key}} mb={3}/>
-        ))}
-      </StyledVerticalCarousel>
-    )}
+    <StyledHorizontalCarousel
+      height={428}
+      width={280}
+      carousel={true}
+      alignItems="center">
+      {reviews.slice(0, 3).map((review, key) => (
+        <Review {...{review, key}} mb={3}/>
+      ))}
+    </StyledHorizontalCarousel>
+    <StyledVerticalCarousel
+      height={448}
+      width={500}
+      carousel={true}
+      ml='45px'>
+      {reviews.slice(0, 3).map((review, key) => (
+        <Review {...{review, key}} mb={3}/>
+      ))}
+    </StyledVerticalCarousel>
+
   </FeedbackList>
 );
 
-export default responsive(({isMobile, ...props}) => (
-  <Reviews {...{...props, isMobile}} />
-));
+export default Reviews;
