bac068fd1484c0431aceb28b419616887fd434b1
kna-github <nikolay.kuznetsov@gmail.com>
2019-10-15 19:16:38 +0300

    fact carousel on media queries;

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Fact from '../components/Fact'
 import Carousel from '../components/Carousel'
 import {space} from 'styled-system';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
-const Facts = ({facts, pStyles, className, carousel}) =>
-  <Carousel width={320} height={225} {...{pStyles, className, carousel}} alignItems='center'>
-    {facts.slice(0, 3).map((fact, key) => <div {...{key}}>
-      <Fact {...{fact}}/>
-    </div>)}
-  </Carousel>
 
-export default styled(Facts)`
-  ${space}
+const commonStyles = css`
+  width: 320px;
+  margin: 140px auto 0;
+  @media(min-width: ${p => p.theme.breakpoints[1]}) {
+    width: 100%;
+  }
 `
+
+const pStyles = css`
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  justify-content: space-around;
+  @media(min-width: ${p => p.theme.breakpoints[3]}) {
+    justify-content: space-between;
+  }
+`;
+
+
+const MobileCarousel = styled(Carousel)`
+  ${commonStyles};
+  @media(min-width: ${p => p.theme.breakpoints[1]}) {
+    display: none;
+  }
+`
+
+const DesktopCarousel = styled(Carousel)`
+  ${commonStyles};
+  @media(max-width: ${p => p.theme.brkpnts[1] - 1}px) {
+    display: none;
+  }
+`
+
+const Facts = ({facts}) => <Fragment>
+    <MobileCarousel width={320} height={225} {...{pStyles, carousel:true}} alignItems='center'>
+      {facts.slice(0, 3).map((fact, key) => <div {...{key}}>
+        <Fact {...{fact}}/>
+      </div>)}
+    </MobileCarousel>
+    <DesktopCarousel width={320} height={225} {...{pStyles, carousel:false}} alignItems='center'>
+      {facts.slice(0, 3).map((fact, key) => <div {...{key}}>
+        <Fact {...{fact}}/>
+      </div>)}
+    </DesktopCarousel>
+  </Fragment>
+
+export default Facts;
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,39 +12,21 @@ import {
 } from '../organisms'
 import background from "../feedback.svg";
 import {withLayout} from "../organisms";
-import {responsive} from "../utils";
 
 const FeedbackWB = withBackground(background, 1133, 686)(({className, mt, reviews}) => <Feedback {...{className, mt, reviews}}/>)`
     left: -230px;
     top: 247px;
 `
 
-const StyledFacts = styled(Facts)`
-  width: 320px;
-  margin: 140px auto 0;
-  @media(min-width: ${p => p.theme.breakpoints[1]}) {
-    width: 100%;
-  }
-`
 
-const pStyles = css`
-  max-width: 1200px;
-  margin-left: auto;
-  margin-right: auto;
-  justify-content: space-around;
-  @media(min-width: ${p => p.theme.breakpoints[3]}) {
-    justify-content: space-between;
-  }
-`;
 
-const SSF = responsive(({isMobile, ...props}) => <StyledFacts carousel={isMobile} {...{...props, pStyles}}/>);
 
 export default withLayout()(withRouteData(({page, services, projects, industries, facts, stages, reviews}) => (
     <Fragment>
 
         <Services mt={[6, 7, 8]} page={page} services={services}/>
 
-        <SSF {...{facts}}/>
+        <Facts {...{facts}}/>
 
         <Industries mt={8} industries={industries}/>
 
