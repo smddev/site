c006c3344bb1a5cdc34a9c5ee6159ccba6ada576
kna-github <nikolay.kuznetsov@smddev.com>
2025-02-19 11:50:34 +0300

    temporary remove layout hoc

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const FeedbackWB = withBackground(background, 1133, 686)(({className, mt, review
     top: 247px;
 `
 
-export default withLayout()(() => { 
+export default () => { 
     const { page, services, projects, industries, facts, stages, reviews } = useRouteData();    
     return (
         <Fragment>
@@ -37,4 +37,4 @@ export default withLayout()(() => {
 
         </Fragment>
     )}
-)
+
